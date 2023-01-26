65ba4227b6736473e10906ceccd7178b2a2671cf
Franciszek Stodulski <you@example.com>
2023-01-26 13:46:54 +0100

    refactor: 💡 update env

--- src/lib/core/providers/server-http-client.provider.ts
+++ src/lib/core/providers/server-http-client.provider.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-import { PUBLIC_HOST_URL } from '$env/static/public';
-
 export const ServerHttpClientProvider = axios.create({
-  baseURL: PUBLIC_HOST_URL,
+  baseURL: process.env.PUBLIC_HOST_URL,
   headers: {
     'Content-type': 'application/json'
   }
